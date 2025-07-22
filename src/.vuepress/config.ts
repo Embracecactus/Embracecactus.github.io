29a1dc6b0aca9bcdfab7e0b6314d195ae011b2dd
li jian <15588296118@163.com>
2025-07-22 23:22:03 +0800

    注释掉英文界面

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -6,16 +6,16 @@ export default defineUserConfig({
   base: "/",
 
   locales: {
-    "/": {
-      lang: "zh-CN",
-      title: "李健的博客",
-      description: "",
-    },
-    // "/zh/": {
+    // "/": {
     //   lang: "zh-CN",
-    //   title: "博客演示",
-    //   description: "vuepress-theme-hope 的博客演示",
+    //   title: "李健的博客",
+    //   description: "",
     // },
+    "/zh/": {
+      lang: "zh-CN",
+      title: "勇闯嵌入式的猪猪侠的博客",
+      description: "勇闯嵌入式的猪猪侠的博客",
+    },
   },
 
   theme,
