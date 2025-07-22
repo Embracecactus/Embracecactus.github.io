83efc4f44a91f512f3c8bc37eb098562910d9b68
li jian <15588296118@163.com>
2025-07-22 23:44:56 +0800

    撤销主页路径

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -3,15 +3,15 @@ import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 
 export default defineUserConfig({
-  base: "/zh/",
+  base: "/",
 
   locales: {
-    "../": {
+    "/": {
       lang: "en-US",
       title: "Blog of ZhuZhuXia Who Adventures in Embedded Systems",
       description: "Blog of ZhuZhuXia Who Adventures in Embedded Systems",
     },
-    "/": {
+    "/zh/": {
       lang: "zh-CN",
       title: "勇闯嵌入式的猪猪侠的博客",
       description: "勇闯嵌入式的猪猪侠的博客",
