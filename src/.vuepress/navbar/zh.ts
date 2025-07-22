7be36f40104587b4bfcf704bc840004a517f92ef
li jian <15588296118@163.com>
2025-07-23 00:00:00 +0800

    删除v2链接

--- src/.vuepress/navbar/zh.ts
+++ src/.vuepress/navbar/zh.ts
@@ -20,7 +20,7 @@ export const zhNavbar = navbar([
         ],
       },
       {
-        text: "香蕉",
+        text: "MCU",
         icon: "pen-to-square",
         prefix: "mcu/",
         children: [
@@ -29,9 +29,9 @@ export const zhNavbar = navbar([
       },
     ],
   },
-  {
-    text: "V2 文档",
-    icon: "book",
-    link: "https://theme-hope.vuejs.press/zh/",
-  },
+  // {
+  //   text: "V2 文档",
+  //   icon: "book",
+  //   link: "https://theme-hope.vuejs.press/zh/",
+  // },
 ]);
