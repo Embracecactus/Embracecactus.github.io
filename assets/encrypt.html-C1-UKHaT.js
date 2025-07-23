1635f90402d1e8d9ba59d9b297eccf77a5d28e7d
Embracecactus <49155304+Embracecactus@users.noreply.github.com>
2025-07-23 13:01:15 +0000

    Deploying to gh-pages from @ Embracecactus/Embracecactus.github.io@14daf1be920755e05252283ee5acecab74b1e845 🚀

---
+++ assets/encrypt.html-C1-UKHaT.js
@@ -0,0 +1,15 @@
+import{_ as e}from"./plugin-vue_export-helper-DlAUqK2U.js";import{c as t,e as n,o}from"./app-kSBQbhiE.js";const r={};function a(c,m){return o(),t("div",null,[n(` ---
+icon: lock
+category:
+  - 使用指南
+tag:
+  - 加密
+---
+
+# 密码加密的文章
+
+实际的文章内容。
+
+段落 1 文字段落 1 文字段落 1 文字段落 1 文字段落 1 文字段落 1 文字段落 1 文字段落 1 文字段落 1 文字段落 1 文字段落 1 文字段落 1 文字。
+
+段落 2 文字段落 2 文字段落 2 文字段落 2 文字段落 2 文字段落 2 文字段落 2 文字段落 2 文字段落 2 文字段落 2 文字段落 2 文字段落 2 文字段落 2 文字段落 2 文字。 `)])}const l=e(r,[["render",a]]),s=JSON.parse('{"path":"/zh/demo/encrypt.html","title":"","lang":"zh-CN","frontmatter":{"head":[["script",{"type":"application/ld+json"},"{\\"@context\\":\\"https://schema.org\\",\\"@type\\":\\"Article\\",\\"headline\\":\\"\\",\\"image\\":[\\"\\"],\\"dateModified\\":\\"2025-07-23T12:56:37.000Z\\",\\"author\\":[{\\"@type\\":\\"Person\\",\\"name\\":\\"GGbond\\",\\"url\\":\\"https://embracecactus.github.io\\"}]}"],["meta",{"property":"og:url","content":"https://embracecactus.github.io/zh/demo/encrypt.html"}],["meta",{"property":"og:site_name","content":"勇闯嵌入式的猪猪侠的博客"}],["meta",{"property":"og:type","content":"article"}],["meta",{"property":"og:locale","content":"zh-CN"}],["meta",{"property":"og:locale:alternate","content":"en-US"}],["meta",{"property":"og:updated_time","content":"2025-07-23T12:56:37.000Z"}],["meta",{"property":"article:modified_time","content":"2025-07-23T12:56:37.000Z"}],["link",{"rel":"alternate","hreflang":"en-us","href":"https://embracecactus.github.io/demo/encrypt.html"}]]},"git":{"createdTime":1753107151000,"updatedTime":1753275397000,"contributors":[{"name":"li jian","username":"","email":"15588296118@163.com","commits":2}]},"readingTime":{"minutes":0.51,"words":154},"filePathRelative":"zh/demo/encrypt.md","excerpt":"<!-- ---\\nicon: lock\\ncategory:\\n  - 使用指南\\ntag:\\n  - 加密\\n---\\n\\n# 密码加密的文章\\n\\n实际的文章内容。\\n\\n段落 1 文字段落 1 文字段落 1 文字段落 1 文字段落 1 文字段落 1 文字段落 1 文字段落 1 文字段落 1 文字段落 1 文字段落 1 文字段落 1 文字。\\n\\n段落 2 文字段落 2 文字段落 2 文字段落 2 文字段落 2 文字段落 2 文字段落 2 文字段落 2 文字段落 2 文字段落 2 文字段落 2 文字段落 2 文字段落 2 文字段落 2 文字。 -->\\n"}');export{l as comp,s as data};
---
+++ assets/layout.html-DAyh3Rr3.js
@@ -0,0 +1,31 @@
+import{_ as e}from"./plugin-vue_export-helper-DlAUqK2U.js";import{c as t,e as h,o as n}from"./app-kSBQbhiE.js";const o={};function s(r,p){return n(),t("div",null,[h(` ---
+title: 布局
+icon: object-group
+order: 2
+category:
+  - 指南
+tag:
+  - 布局
+---
+
+布局包括:
+
+- [导航栏](https://theme-hope.vuejs.press/zh/guide/layout/navbar.html)
+- [侧边栏](https://theme-hope.vuejs.press/zh/guide/layout/sidebar.html)
+- [页脚](https://theme-hope.vuejs.press/zh/guide/layout/footer.html)
+
+同时每个页面包含:
+
+- [路径导航](https://theme-hope.vuejs.press/zh/guide/layout/breadcrumb.html)
+- [标题和页面信息](https://theme-hope.vuejs.press/zh/guide/feature/page-info.html)
+- [TOC (文章标题列表)](https://theme-hope.vuejs.press/zh/guide/layout/page.html#标题列表)
+- [贡献者、更新时间等页面元信息](https://theme-hope.vuejs.press/guide/feature/meta.html)
+- [评论](https://theme-hope.vuejs.press/zh/guide/feature/comment.html)
+
+主题也带有以下元素:
+
+- [夜间模式按钮](https://theme-hope.vuejs.press/zh/guide/interface/darkmode.html)
+- [返回顶部按钮](https://theme-hope.vuejs.press/guide/interface/others.html#返回顶部按钮)
+- [打印按钮](https://theme-hope.vuejs.press/guide/interface/others.html#打印按钮)
+
+你可以在主题选项和页面的 frontmatter 中自定义它们。 `)])}const u=e(o,[["render",s]]),i=JSON.parse('{"path":"/zh/demo/layout.html","title":"","lang":"zh-CN","frontmatter":{"head":[["script",{"type":"application/ld+json"},"{\\"@context\\":\\"https://schema.org\\",\\"@type\\":\\"Article\\",\\"headline\\":\\"\\",\\"image\\":[\\"\\"],\\"dateModified\\":\\"2025-07-23T12:56:37.000Z\\",\\"author\\":[{\\"@type\\":\\"Person\\",\\"name\\":\\"GGbond\\",\\"url\\":\\"https://embracecactus.github.io\\"}]}"],["meta",{"property":"og:url","content":"https://embracecactus.github.io/zh/demo/layout.html"}],["meta",{"property":"og:site_name","content":"勇闯嵌入式的猪猪侠的博客"}],["meta",{"property":"og:type","content":"article"}],["meta",{"property":"og:locale","content":"zh-CN"}],["meta",{"property":"og:locale:alternate","content":"en-US"}],["meta",{"property":"og:updated_time","content":"2025-07-23T12:56:37.000Z"}],["meta",{"property":"article:modified_time","content":"2025-07-23T12:56:37.000Z"}],["link",{"rel":"alternate","hreflang":"en-us","href":"https://embracecactus.github.io/demo/layout.html"}]]},"git":{"createdTime":1753107151000,"updatedTime":1753275397000,"contributors":[{"name":"li jian","username":"","email":"15588296118@163.com","commits":2}]},"readingTime":{"minutes":0.53,"words":159},"filePathRelative":"zh/demo/layout.md","excerpt":"<!-- ---\\ntitle: 布局\\nicon: object-group\\norder: 2\\ncategory:\\n  - 指南\\ntag:\\n  - 布局\\n---\\n\\n布局包括:\\n\\n- [导航栏](https://theme-hope.vuejs.press/zh/guide/layout/navbar.html)\\n- [侧边栏](https://theme-hope.vuejs.press/zh/guide/layout/sidebar.html)\\n- [页脚](https://theme-hope.vuejs.press/zh/guide/layout/footer.html)\\n\\n同时每个页面包含:\\n\\n- [路径导航](https://theme-hope.vuejs.press/zh/guide/layout/breadcrumb.html)\\n- [标题和页面信息](https://theme-hope.vuejs.press/zh/guide/feature/page-info.html)\\n- [TOC (文章标题列表)](https://theme-hope.vuejs.press/zh/guide/layout/page.html#标题列表)\\n- [贡献者、更新时间等页面元信息](https://theme-hope.vuejs.press/guide/feature/meta.html)\\n- [评论](https://theme-hope.vuejs.press/zh/guide/feature/comment.html)\\n\\n主题也带有以下元素:\\n\\n- [夜间模式按钮](https://theme-hope.vuejs.press/zh/guide/interface/darkmode.html)\\n- [返回顶部按钮](https://theme-hope.vuejs.press/guide/interface/others.html#返回顶部按钮)\\n- [打印按钮](https://theme-hope.vuejs.press/guide/interface/others.html#打印按钮)\\n\\n你可以在主题选项和页面的 frontmatter 中自定义它们。 -->"}');export{u as comp,i as data};
